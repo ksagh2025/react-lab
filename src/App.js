56e3383739f8b42cc61883975ea8a83d0b9b397a
Student <student@d17-426b-pc-08.ipa.ii.agh.edu.pl>
2026-05-16 14:20:50 +0200

    drobne poprawki

--- src/App.js
+++ src/App.js
@@ -6,35 +6,7 @@ import {useState} from 'react';
 import UserPanel from "./UserPanel";
 
 function App() {
-    // const [email, setEmail] = useState('fracz@agh.edu.pl');
-    const [loggedInUsername, setIsLoggedInUsername] = useState(null);
-
-    // function handleChange(event) {
-    //     setEmail(event.target.value);
-    // }
-
-    // function handleClick(){
-    //     if (loggedInUsername === false && email.length>0)
-    //     {setLoggedInUsername(email)}
-    //     else {setLoggedInUsername(email);}
-    // }
-
-    // function handleLogin(email){
-    //     setIsLoggedIn(true);
-    //     setEmail(email);
-    // }
-
-    // let message;
-    // if (email.length < 5) {
-    //     message = <div>Ale masz krótki adres!</div>;
-    // } else if (email.length < 10) {
-    //     message = <div>Twój adres e-mail jest w sam raz.</div>;
-    // } else {
-    //     message = <div>Twój adres e-mail jest stanowczo za długi.</div>;
-    // }
-    //
-    // let logMessage;
-    // let logHello = 'Witaj '+ email;
+    const [loggedInUsername, setLoggedInUsername] = useState(null);
 
   return (
 
@@ -42,22 +14,9 @@ function App() {
           <h1>System do zapisów na zajęcia</h1>
           {
               loggedInUsername ?
-                  <UserPanel email={loggedInUsername} onLogout={() => setIsLoggedInUsername(null)} /> :
-                  <LoginForm onLogin={(email) => setIsLoggedInUsername(email)} />
+                  <UserPanel email={loggedInUsername} onLogout={() => setLoggedInUsername(null)} /> :
+                  <LoginForm onLogin={(email) => setLoggedInUsername(email)} />
           }
-          {/*{!isLoggedIn && <LoginForm onLogin ={handleLogin} />}*/}
-
-          {/*{isLoggedIn && <UserPanel email={email} onLogout={() => setIsLoggedIn(false)} />*/}
-          {/*// <div>*/}
-          {/*// <h1>Witaj {email}</h1>*/}
-          {/*// <button type="button" onClick={handleClick}>Wyloguj</button>*/}
-          {/*//     <br/><br/>*/}
-          {/*// <a href="#" onClick={() => handleClick()}> Wyloguj </a>*/}
-          {/*//     <br/><br/>*/}
-          {/*// </div>*/}
-          {/*}*/}
-          {/*<br/>*/}
-          {/*{logMessage}*/}
 
 
       </div>
