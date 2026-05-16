72666573a864eaee9c7c35b3592c886e4233b9eb
Student <student@d17-426b-pc-08.ipa.ii.agh.edu.pl>
2026-05-16 10:15:24 +0200

    pierwszy commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    let email = 'ReactTest@agh.edu.pl';
+
+    function handleChange(event) {
+        console.log(event.target.value);
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1>System do zapisów na zajęcia</h1>
+        <h2>Twój e-mail to {email.toUpperCase()}</h2>
+        <h3>{2+2}</h3>
+        <input type="text" onChange={handleChange}/>
     </div>
   );
 }
