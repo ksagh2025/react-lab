7f4d2e8653c06748e94db87426e4656d04db6311
krzysiek2020 <krzysiek.s.tel@gmail.com>
2026-05-16 21:55:38 +0200

    dodawanie zajec

--- src/UserPanel.js
+++ src/UserPanel.js
@@ -1,10 +1,15 @@
+import NewMeetingForm from "./NewMeetingForm";
+import MeetingsList from "./MeetingsList";
+import MeetingsPage from "./MeetingsPage";
+
 export default function UserPanel({email, onLogout}) {
 
     return(
         <div>
             <h1>Witaj {email}</h1>
             <a onClick={() => onLogout()}> Wyloguj </a>
-            <br/><br/>
+            <br/>
+            <MeetingsPage />
         </div>
     );
 
